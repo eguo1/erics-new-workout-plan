91f17dda57866c66fe50afc39e874dba9bccd7d5
Eric Guo <ericxguo@gmail.com>
2018-05-31 11:50:19 -0500

    created express backend skeleton

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  entry: './client/main.jsx',
+  mode: isProd ? 'production' : 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
